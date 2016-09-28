4efa1ccf6f7b4060e6acef870e7db67b5af97071
Jim Lin <jimlintw922@gmail.com>
2016-09-28 10:03:08 +0000

    init main-app.po; update server.py

--- main_app/cmd/server.py
+++ main_app/cmd/server.py
@@ -2,4 +2,9 @@ def main():
     from main_app import settings
     CONF = settings.load_conf_file()
 
-    print(CONF.default.bind_host)
+    import gettext
+    locale_path = '/home/pi/py-proj/main_app/locale/'
+    zh_trans = gettext.translation('main-app', locale_path, languages=['zh_TW'])
+    en_trans = gettext.translation('main-app', locale_path, languages=['en_US'])
+    en_trans.install()
+    print('%s %s' % (_('listening'), CONF.default.bind_host))
