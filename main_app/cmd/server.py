5bce4ed2c1997f6c7d965e85156ccf7af0ec493c
Jim Lin <jimlintw922@gmail.com>
2016-09-29 13:34:23 +0000

    üpdate setttings.py server.py for introducing logging

--- main_app/cmd/server.py
+++ main_app/cmd/server.py
@@ -3,4 +3,6 @@ def main():
     CONF = settings.load_conf_file()
 
     from main_app.i18n import _
-    print('%s %s' % (_('listening'), CONF.default.bind_host))
+    import logging
+    logger = logging.getLogger(__name__)
+    logger.info('%s %s' % (_('listening'), CONF.default.bind_host))
--- main_app/settings.py
+++ main_app/settings.py
@@ -14,3 +14,36 @@ CONF.register_opts(default_opts, default_group)
 def load_conf_file():
     CONF(default_config_files=['/etc/main-app/app.conf'])
     return CONF
+
+
+import logging.config
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'main_app': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    }
+}
+
+logging.config.dictConfig(LOGGING)
